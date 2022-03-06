35f29e6e669d1072334bfb3ad60b20a2dead1aad
kateganteng <101029958+kateganteng@users.noreply.github.com>
2022-03-06 12:54:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,8 +48,8 @@ global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '©KATEGANTENG' //change the watermark 
-global.image = 'https://telegra.ph/file/1a3eac3701775793618b3.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg'
+global.image = 'https://telegra.ph/file/7a9220e4d9c5baacf2195.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/7a9220e4d9c5baacf2195.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
