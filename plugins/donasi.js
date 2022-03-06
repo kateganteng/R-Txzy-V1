e81816a9779e28f63a699d3421c707bade013308
kateganteng <101029958+kateganteng@users.noreply.github.com>
2022-03-06 12:20:32 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,9 +3,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ GoPay: 085730903853 [AlyaXd]
-├ Dana: 082221792667 [BOTCAHX]
-├ Pulsa: 081395861695 [BOTCAHX]
+├ Ovo : 085954944195
+├ Dana : 085954944195
+├ Owner : wa.me/6285954944195
 └────
 `.trim(), watermark, 'Menu', '.menu', m)
 handler.help = ['donasi']
